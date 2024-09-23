6ab445504ed4bb8f01d291859fb069791e8a636e
robinho <robinsonmelo543@gmail.com>
2024-09-23 18:21:26 -0300

    comit

---
+++ aula05/script.js
@@ -0,0 +1,4 @@
+const h1 = Number(window.prompt("Digite o primeiro valor "));
+const h2 = Number(window.prompt("Digite o segundo valor"));
+var res = n1 + n2;
+window.alert('o resultado de $(n1) + $(n2) = $(res)');
